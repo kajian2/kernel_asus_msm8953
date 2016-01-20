716bb26f8f03fec51b5a037fb4df2de26740d2f2
Tim Murray <timmurray@google.com>
2016-01-19 16:36:40 -0800

    ANDROID: mmc: move to a SCHED_FIFO thread
    (cherry picked from commit 011e507b413393eab8279dac8b778ad9b6e9971b)
    
    Running mmcqd as a prio 120 thread forces it to compete with standard
    user processes for IO performance, especially when the system is under
    severe CPU load. Move it to a SCHED_FIFO thread to reduce the impact of
    load on IO performance.
    
    Signed-off-by: Tim Murray <timmurray@google.com>
    Bug: 25392275
    Change-Id: I1edfe73baa25e181367c30c1f40fee886e92b60d

--- drivers/mmc/card/queue.c
+++ drivers/mmc/card/queue.c
@@ -21,6 +21,7 @@
 
 #include <linux/mmc/card.h>
 #include <linux/mmc/host.h>
+#include <linux/sched/rt.h>
 #include "queue.h"
 //ASUS_BSP Deeo : mmc suspend stress test +++
 #ifdef CONFIG_MMC_SUSPENDTEST
@@ -144,6 +145,11 @@ static inline void mmc_cmdq_ready_wait(struct mmc_host *host,
 {
 	struct mmc_cmdq_context_info *ctx = &host->cmdq_ctx;
 	struct request_queue *q = mq->queue;
+	struct sched_param scheduler_params = {0};
+
+	scheduler_params.sched_priority = 1;
+
+	sched_setscheduler(current, SCHED_FIFO, &scheduler_params);
 
 	/*
 	 * Wait until all of the following conditions are true:
